857a44063df869a0f3436cefdc00de3cb5f30eb2
Melvin MIAUX <woolfy.rl@gmail.com>
2025-04-28 10:16:35 +0200

    modif output

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,11 @@
 from transformers import pipeline
 classifier = pipeline("sentiment-analysis", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
 
-print("Modèle POSITIF")
+print("Modèle NEUTRE : The central bank held its interest rate steady at 5%")
+print(classifier("The central bank held its interest rate steady at 5%"))
+
+print("Modèle POSITIF : Tesla's stock price surges after strong earnings")
 print(classifier("Tesla's stock price surges after strong earnings."))
 
-print ("Modèle NEGATIF")
+print ("Modèle NEGATIF : The financial crisis is expected to worsen in the coming months")
 print(classifier("The financial crisis is expected to worsen in the coming months"))
-
-print("Modèle NEUTRE")
-print("The central bank held its interest rate steady at 5%")
